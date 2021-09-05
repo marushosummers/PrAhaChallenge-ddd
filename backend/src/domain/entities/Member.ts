cf43c87cdbaaef75d1dd2eff50aac60a35bbb868
marusho <mrsho34@gmail.com>
2021-09-05 19:06:48 +0900

    メールアドレスの形式バリデーションを実装

--- backend/src/domain/entities/Member.ts
+++ backend/src/domain/entities/Member.ts
@@ -6,7 +6,9 @@ export class Member {
   private activityStatus: ActivityStatus // TODO: ActivityStatusはenumとして定義した。DBでの定義は削除する。
 
   public constructor(props: { id: string, name: string, email: string, pair: string}) {
-    const { id, name, email, pair} = props
+    const { id, name, email, pair } = props
+    this.validateEmail(email)
+
     this.id = id
     this.name = name
     this.email = email
@@ -21,6 +23,12 @@ export class Member {
       email: this.email,
       pair: this.pair,
       activityStatus: this.activityStatus
+    }
+  }
+
+  private validateEmail(email: string): void {
+    if (!new RegExp("^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$").test(email)) {
+      throw new Error("Invalid Email Format");
     }
   }
 }
