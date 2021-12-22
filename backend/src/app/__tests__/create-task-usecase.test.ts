20b6f21c358a5a995132993f92fbf27d29893720
marusho <mrsho34@gmail.com>
2021-12-22 16:43:07 +0900

    add test: create task usecaseの正常系テストを追加

---
+++ backend/src/app/__tests__/create-task-usecase.test.ts
@@ -0,0 +1,46 @@
+import * as faker from 'faker'
+import { PrismaClient } from '@prisma/client'
+import { Task } from 'src/domain/entities/Task'
+import { Member, MemberTask } from 'src/domain/entities/Member'
+import { CreateTaskUseCase } from '../create-task-usecase'
+import { TaskRepository } from '../../infra/db/repository/task-repository'
+import { MemberRepository } from 'src/infra/db/repository/member-repository'
+import { mocked } from 'ts-jest/utils'
+import { MockedObjectDeep } from 'ts-jest/dist/utils/testing'
+
+jest.mock('@prisma/client')
+jest.mock('src/infra/db/repository/task-repository.ts')
+jest.mock('src/infra/db/repository/member-repository.ts')
+
+describe('do', () => {
+  describe('OK', () => {
+    let mockTaskRepository: MockedObjectDeep<TaskRepository>
+    let mockMemberRepository: MockedObjectDeep<MemberRepository>
+    let mockTask: MockedObjectDeep<Task>
+    let mockMember: MockedObjectDeep<Member>
+    beforeAll(() => {
+      const prisma = new PrismaClient()
+      mockTaskRepository = mocked(new TaskRepository(prisma), true)
+      mockMemberRepository = mocked(new MemberRepository(prisma), true)
+    })
+    it('例外が発生しない', async () => {
+      const taskId = faker.datatype.uuid();
+      const content = "testContent"
+      const memberId = faker.datatype.uuid();
+      const name = "testMemberName"
+      const email = faker.internet.email()
+      const activityStatus = "ONGOING"
+      const memberTasks: MemberTask[] = []
+      const expectedReponse = new Task({ id: taskId, content: content })
+
+      mockTask = mocked(new Task({ id: taskId, content: content}), true)
+      mockMember = mocked(new Member({ id: memberId, name: name, email: email, activityStatus: activityStatus, memberTasks: memberTasks}), true)
+      mockTaskRepository.save.mockResolvedValueOnce(mockTask)
+      mockMemberRepository.save.mockResolvedValueOnce([mockMember])
+      mockMemberRepository.getAll.mockResolvedValueOnce([mockMember])
+
+      const usecase = new CreateTaskUseCase(mockTaskRepository, mockMemberRepository)
+      return expect(usecase.do({ content: content })).resolves.toStrictEqual(expectedReponse)
+    })
+  })
+})
